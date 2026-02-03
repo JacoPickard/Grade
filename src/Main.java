24091514f189d5bc1728a058ef57d58e0cb91d27
W0769269 <jacopickard@gmail.com>
2026-02-03 11:04:41 -0600

    In class work

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Grading Program");
+        char choice;
+        do {
+            numberGradeToLetterGrade(scanner);
+            System.out.print("Do you want to enter another grade? (y/n): ");
+            choice = scanner.next().toLowerCase().charAt(0);
+        }
+        while (choice == 'y');
+        scanner.close();
+        System.out.println("Program ended.");
+    }
+
+    public static void numberGradeToLetterGrade(Scanner scanner) {
+        System.out.println("Grading Progra");
         System.out.println("Provides letter grade given number grade.");
-        System.out.println("Please input a number grade: ");
-        double numberGrade = 89;
-        numberGrade = scanner.nextDouble();
+        System.out.print("Please input a number grade: ");
+        double numberGrade = scanner.nextDouble();
         System.out.println("Number grade is " + numberGrade);
-        if (numberGrade >= 90){
+        if (numberGrade >= 90) {
             System.out.println("You get an A");
         }
-        else if (numberGrade >= 80){
+        else if (numberGrade >= 80) {
             System.out.println("You get a B");
         }
-        else if (numberGrade >= 70){
+        else if (numberGrade >= 70) {
             System.out.println("You get a C");
         }
-        else if (numberGrade >= 60){
+        else if (numberGrade >= 60) {
             System.out.println("You get a D");
         }
-        else{
-            System.out.println("I can't give your letter grade");
+        else if (numberGrade >= 0) {
+            System.out.println("You get an F");
         }
-
+        else {
+            System.out.println("Invalid grade entered");
+        }
     }
 }
